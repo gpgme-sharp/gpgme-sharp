ccb582c06e7a3613ee119410c916f87b0cba6979
Daniel Lo Nigro <d@d.sb>
2019-02-10 12:57:37 -0800

    Mark all delegates are cdecl, as other methods have the same bug.

--- GPGME.Native.Shared/libgpgme_Delegates.cs
+++ GPGME.Native.Shared/libgpgme_Delegates.cs
@@ -13,7 +13,7 @@ namespace Libgpgme.Interop
         int fd);
 
     /* Inform the user about progress made.  */
-
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void gpgme_progress_cb_t(
         IntPtr opaque,
         IntPtr what,
@@ -24,7 +24,7 @@ namespace Libgpgme.Interop
     /* Read up to SIZE bytes into buffer BUFFER from the data object with
        the handle HANDLE.  Return the number of characters read, 0 on EOF
        and -1 on error.  If an error occurs, errno is set.  */
-
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate IntPtr gpgme_data_read_cb_t(
         IntPtr handle,
         IntPtr buffer,
@@ -33,7 +33,7 @@ namespace Libgpgme.Interop
     /* Write up to SIZE bytes from buffer BUFFER to the data object with
        the handle HANDLE.  Return the number of characters written, or -1
        on error.  If an error occurs, errno is set.  */
-
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate IntPtr gpgme_data_write_cb_t(
         IntPtr handle,
         IntPtr buffer,
@@ -42,7 +42,7 @@ namespace Libgpgme.Interop
     /* Set the current position from where the next read or write starts
        in the data object with the handle HANDLE to OFFSET, relativ to
        WHENCE.  */
-
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate IntPtr gpgme_data_seek_cb_t(
         IntPtr handle,
         IntPtr offset,
@@ -51,19 +51,19 @@ namespace Libgpgme.Interop
     /* Set the current position from where the next read or write starts
        in the data object with the handle HANDLE to OFFSET, relativ to
        WHENCE.  */
-
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate long gpgme_data_seek_cb_t_lfs(
         IntPtr handle,
         long offset,
         int whence);
 
     /* Close the data object with the handle DL.  */
-
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate void gpgme_data_release_cb_t(
         IntPtr handle);
 
     /* Interact with the user about an edit operation.  */
-
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
     public delegate int gpgme_edit_cb_t(
         IntPtr opaque, // void *
         int status, //gpgme_status_code_t
