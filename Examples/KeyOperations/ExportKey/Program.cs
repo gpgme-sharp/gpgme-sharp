46d150e81fd1ad748b3c5067d2c9b0431de599c5
Daniel Lo Nigro <d@d.sb>
2019-02-24 20:06:29 -0800

    Add export modes and example for export

---
+++ Examples/KeyOperations/ExportKey/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Libgpgme;
+
+namespace ExportKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string keyToExport = "A3E1D41E632142737E3EDC340E6B9269F9F982C1!";
+
+            using (var ctx = new Context())
+            {
+                ctx.Armor = true;
+                ctx.KeyStore.Export(keyToExport, "exported.pub");
+                Console.WriteLine($"Exported public key {keyToExport} to exported.pub");
+
+                ctx.KeyStore.Export(keyToExport, "exported.gpg", ExportMode.Secret);
+                Console.WriteLine($"Exported private key {keyToExport} (if available) to exported.gpg");
+            }
+        }
+    }
+}
